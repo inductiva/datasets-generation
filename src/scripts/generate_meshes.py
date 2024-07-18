012ec153be05f320086f2a62a93144fd8b9fbcb0
rcvalerio <rvalerio@inductiva.ai>
2024-07-18 10:14:13 +0100

    fix pylint

--- src/scripts/generate_meshes.py
+++ src/scripts/generate_meshes.py
@@ -31,14 +31,15 @@ def generate_meshes(input_path, config_file):
     files = os.listdir(input_path)
     image_files = sorted(
         [f for f in files if f.endswith('.jpg') or f.endswith('.png')],
-        key=natural_sort_key)
+        key=natural_sort_key,
+    )
 
     for i, filename in tqdm(enumerate(image_files)):
         image_path = os.path.join(input_path, filename)
         filename_no_ext = os.path.splitext(filename)[0]
-        obj_path = os.path.join(MESHES_DIR, f"{filename_no_ext}.obj")
-        obj_path_renamed = os.path.join(MESHES_DIR, f"mesh_{i}.obj")
-        processed_obj_path = os.path.join(MESHES_PROCESSED_DIR, f"mesh_{i}.obj")
+        obj_path = os.path.join(MESHES_DIR, f'{filename_no_ext}.obj')
+        obj_path_renamed = os.path.join(MESHES_DIR, f'mesh_{i}.obj')
+        processed_obj_path = os.path.join(MESHES_PROCESSED_DIR, f'mesh_{i}.obj')
         command = COMMAND_TEMPLATE.format(config=config_file,
                                           input_file=image_path,
                                           output_path=OUTPUT_PATH)
@@ -46,14 +47,14 @@ def generate_meshes(input_path, config_file):
         try:
             subprocess.run(command, shell=True, check=True)
             os.rename(obj_path, obj_path_renamed)
-            print(f"Successfully generated mesh_{i}.obj")
+            print(f'Successfully generated mesh_{i}.obj')
 
             if PREPROCESS_ENABLED:
                 preprocess_mesh(obj_path, processed_obj_path)
-                print(f"Successfully processed mesh_{i}.obj")
+                print(f'Successfully processed mesh_{i}.obj')
 
         except subprocess.CalledProcessError as e:
-            print(f"Error occurred while processing {filename}: {e}")
+            print(f'Error occurred while processing {filename}: {e}')
 
 
 if __name__ == '__main__':
