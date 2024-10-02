d4fdb6afcca29e922ac817c7ca8e2331bf3dfb88
rcvalerio <rvalerio@inductiva.ai>
2024-10-02 11:19:18 +0100

    Refactor generate_meshes.py to use absl flags

--- windtunnel_dataset/scripts/generate_meshes.py
+++ windtunnel_dataset/scripts/generate_meshes.py
@@ -4,20 +4,23 @@ import os
 import re
 import subprocess
 
-from preprocess_meshes import preprocess_mesh
+from absl import app, flags, logging
+import postprocess_mesh
 from tqdm import tqdm
 
-# Constants
-INPUT_PATH = 'data/cars196'
-CONFIG_FILE = 'configs/instant-mesh-large.yaml'
-OUTPUT_PATH = 'data/output'
-OUTPUT_SUBDIR = 'instant-mesh-large'
-MESHES_DIR = os.path.join(OUTPUT_PATH, OUTPUT_SUBDIR, 'meshes')
-MESHES_PROCESSED_DIR = os.path.join(OUTPUT_PATH, OUTPUT_SUBDIR,
-                                    'meshes_processed')
-COMMAND_TEMPLATE = ('python run.py {config} {input_file} --save_video '
-                    '--output_path {output_path}')
-PREPROCESS_ENABLED = True
+FLAGS = flags.FLAGS
+flags.DEFINE_string("input_path", "data/cars196", "Path to the input data.")
+flags.DEFINE_string(
+    "config_file", "configs/instant-mesh-large.yaml", "Path to the config file."
+)
+flags.DEFINE_string("output_path", "data/output", "Path to the output directory.")
+flags.DEFINE_string("output_subdir", "instant-mesh-large", "Subdirectory for output.")
+flags.DEFINE_bool("postprocess_meshes", True, "Post process meshes.")
+flags.DEFINE_bool("debug", False, "Enable debug logging.")
+
+COMMAND_TEMPLATE = (
+    "python run.py {config} {input_file} --save_video " "--output_path {output_path}"
+)
 
 
 def natural_sort_key(s):
@@ -27,36 +30,53 @@ def natural_sort_key(s):
     ]
 
 
-def generate_meshes(input_path, config_file):
-    files = os.listdir(input_path)
+def generate_meshes(meshes_dir):
+    meshes_dir = os.path.join(FLAGS.output_path, FLAGS.output_subdir, "meshes")
+    meshes_processed_dir = os.path.join(
+        FLAGS.output_path, FLAGS.output_subdir, "meshes_processed"
+    )
+
+    os.makedirs(meshes_processed_dir, exist_ok=True)
+
+    files = os.listdir(FLAGS.input_path)
     image_files = sorted(
         [f for f in files if f.endswith('.jpg') or f.endswith('.png')],
         key=natural_sort_key,
     )
 
     for i, filename in tqdm(enumerate(image_files)):
-        image_path = os.path.join(input_path, filename)
+        image_path = os.path.join(FLAGS.input_path, filename)
         filename_no_ext = os.path.splitext(filename)[0]
-        obj_path = os.path.join(MESHES_DIR, f'{filename_no_ext}.obj')
-        obj_path_renamed = os.path.join(MESHES_DIR, f'mesh_{i}.obj')
-        processed_obj_path = os.path.join(MESHES_PROCESSED_DIR, f'mesh_{i}.obj')
-        command = COMMAND_TEMPLATE.format(config=config_file,
-                                          input_file=image_path,
-                                          output_path=OUTPUT_PATH)
+        obj_path = os.path.join(meshes_dir, f"{filename_no_ext}.obj")
+        obj_path_renamed = os.path.join(meshes_dir, f"mesh_{i}.obj")
+        processed_obj_path = os.path.join(meshes_processed_dir, f"mesh_{i}.obj")
+        command = COMMAND_TEMPLATE.format(
+            config=FLAGS.config_file,
+            input_file=image_path,
+            output_path=FLAGS.output_path,
+        )
 
         try:
             subprocess.run(command, shell=True, check=True)
             os.rename(obj_path, obj_path_renamed)
             print(f'Successfully generated mesh_{i}.obj')
 
-            if PREPROCESS_ENABLED:
-                preprocess_mesh(obj_path, processed_obj_path)
+            if FLAGS.postprocess_meshes:
+                postprocess_mesh.postprocess_mesh(obj_path, processed_obj_path)
                 print(f'Successfully processed mesh_{i}.obj')
 
         except subprocess.CalledProcessError as e:
             print(f'Error occurred while processing {filename}: {e}')
 
 
-if __name__ == '__main__':
-    os.makedirs(MESHES_PROCESSED_DIR, exist_ok=True)
-    generate_meshes(INPUT_PATH, CONFIG_FILE)
+def main(_):
+    if FLAGS.debug:
+        logging.set_verbosity(logging.DEBUG)
+    else:
+        logging.set_verbosity(logging.INFO)
+
+    generate_meshes()
+
+
+if __name__ == "__main__":
+    app.run(main)
